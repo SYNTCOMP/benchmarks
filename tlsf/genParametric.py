6f48fda57f790f0418470f5089a346ebc2be3f96
Philipp Schlehuber <philipp.schlehuber@epita.fr>
2023-07-18 15:15:05 +0200

    feat: Generating benchmarks now exports a structure
    This structure can be used to run more detailed analysis.
    See the corresponding changes in scripts/inforeal_detailed.py
    
    * genParametric.py,
    selectBenchmarks.py,
    utils.py: Here

--- tlsf/genParametric.py
+++ tlsf/genParametric.py
@@ -3,61 +3,72 @@
 import csv
 import os
 import re
-import sys
+import sys, os
+
+import pandas
+
+from utils import sortStrList
+import pandas as pd
 
 # changed RE to account for comments after the semicolon
 assignREStr = r"^\s*{}\s*=\s*(.*)\s*;.*$"
 
 
 def generateParametric(templateFName, values, tags, outFName):
-    out = open(outFName +
-               "_".join([values[k] for k in sorted(values)]) +
-               ".tlsf", "w")
-    valREs = {k: re.compile(assignREStr.format(k)) for k in values}
-    with open(templateFName, "r") as template:
-        for line in template:
-            for k in values:
-                res = valREs[k].match(line)
-                if res is not None:
-                    line = f"{k} = {values[k]};\n"
-                    break
-            out.write(line)
-    # we also write all tags we know of
-    out.write("//#!SYNTCOMP\n")
-    for k in tags:
-        out.write(f"//{k} : {tags[k]}\n")
-    out.write("//#\n")
-    out.close()
+    fullName = outFName + \
+               "_".join([v for (_, v) in values]) + \
+               ".tlsf" 
+    with open(fullName, "w") as out:
+        valREs = {k: re.compile(assignREStr.format(k)) for (k, _) in values}
+        with open(templateFName, "r") as template:
+            for line in template:
+                for k, v in values:
+                    res = valREs[k].match(line)
+                    if res is not None:
+                        line = f"{k} = {v};\n"
+                        break
+                out.write(line)
+        # we also write all tags we know of
+        out.write("//#!SYNTCOMP\n")
+        for k in tags:
+            out.write(f"//{k} : {tags[k]}\n")
+        out.write("//#\n")
+    return [os.path.basename(fullName)]
 
 
 def generateFromCSV(templateFName, csvFName, outFName):
+    info = []
     with open(csvFName, "r") as csvfile:
-        reader = csv.reader(csvfile, delimiter=',')
-        lineCount = 0
-        for row in reader:
-            if lineCount == 0:
-                header = row.copy()
-            else:
-                vals = dict(zip(header, row))
-                tags = dict()
-                if "status" in vals:
-                    tags["STATUS"] = vals["status"]
-                if "refsize" in vals:
-                    tags["REF_SIZE"] = vals["refsize"]
-                nontags = {k: vals[k] for k in vals
-                           if k not in ["status", "refsize"]}
-                generateParametric(templateFName, nontags, tags, outFName)
-            lineCount += 1
+        df = pandas.read_csv(csvfile)
+        sortNames = list(df.columns)
+        sortNames.remove("status")
+        sortNames.remove("refsize")
+
+        df = df.sort_values(sortNames)
+        header = list(df.columns)
+
+        for _, row in df.iterrows():
+            vals = [(h,r) for h,r in zip(header, row)]
+            tags = dict()
+            if "status" in header:
+                tags["STATUS"] = row.status
+            if "refsize" in header:
+                tags["REF_SIZE"] = row["refsize"]
+            nontags = [(k, str(v)) for (k, v) in vals if k not in ["status", "refsize"]]
+            info += generateParametric(templateFName, nontags, tags, outFName)
+    return info
 
 
 def generateAllParametric(inputRoot, outputRoot):
     root = os.path.join(inputRoot, "parametric")
-    for filename in os.listdir(root):
+    info = []
+    for filename in sortStrList(list(os.listdir(root))):
         if filename.endswith(".tlsf"):
             fullname = os.path.join(root, filename)
             print(f"Found template {fullname}")
-            generateFromCSV(fullname, fullname[:-5] + ".csv",
-                            os.path.join(outputRoot, filename[:-5]))
+            info += generateFromCSV(fullname, fullname[:-5] + ".csv",
+                                    os.path.join(outputRoot, filename[:-5]))
+    return info
 
 
 if __name__ == "__main__":
--- tlsf/selectBenchmarks.py
+++ tlsf/selectBenchmarks.py
@@ -3,10 +3,27 @@
 import os
 import shutil
 import sys
+import argparse
 
 from genParametric import generateAllParametric
+from utils import sortStrList
 
+<<<<<<< HEAD
 benchdirs = ["amba/amba",
+=======
+parser = argparse.ArgumentParser("Create all tlsf instances")
+
+parser.add_argument("benchmarkpath", type=str, help="the path to the benchmark-family directories")
+parser.add_argument("outputpath", type=str, help="the path where you want the benchmarks")
+parser.add_argument("-s", "--savestruct", action="store_true",
+                    help="If set, creates a structure.json containing information "\
+                    "about the benchmark families.")
+
+
+
+
+benchdirs = ["amba/amba", # PSC: I suppose?
+>>>>>>> 4c8d3d2 (feat: Generating benchmarks now exports a structure)
              "amba/amba_decomposed",
              "amba/amba_gr1/specs",
              "arbiters_s4",
@@ -41,24 +58,31 @@ benchdirs = ["amba/amba",
              ]
 
 
-def makeBenchmarks(inputRoot, outputRoot):
+def makeBenchmarks(args):
+    inputRoot = args.benchmarkpath
+    outputRoot = args.outputpath
+    info = {}
     for d in benchdirs:
         root = os.path.join(inputRoot, d)
         hasParametric = False
-        for filename in os.listdir(root):
+        info[d] = []
+        for filename in sortStrList(list(os.listdir(root))):
             if filename == "parametric":
                 print("Found parametric benchmarks!")
                 hasParametric = True
             elif filename.endswith(".tlsf"):
                 fullname = os.path.join(root, filename)
                 print(f"Found benchmark {fullname}")
+                info[d].append(filename)
                 shutil.copy(fullname,
                             os.path.join(outputRoot, filename))
         if hasParametric:
-            generateAllParametric(root, outputRoot)
+            info[d] = generateAllParametric(root, outputRoot)
+    return info
 
 
 if __name__ == "__main__":
+<<<<<<< HEAD
     if len(sys.argv) != 3:
         print("Two positional arguments expected: \n"
               "  (1) the path to the benchmark-family directories\n"
@@ -69,3 +93,13 @@ if __name__ == "__main__":
     else:
         makeBenchmarks(sys.argv[1], sys.argv[2])
         exit(0)
+=======
+    args = parser.parse_args()
+    info = makeBenchmarks(args)
+    if args.savestruct:
+        import json
+        with open(os.path.join(args.outputpath, "structure.json"), "w") as fp:
+            json.dump(info, fp)
+
+    exit(0)
+>>>>>>> 4c8d3d2 (feat: Generating benchmarks now exports a structure)
