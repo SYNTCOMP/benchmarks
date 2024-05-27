cb284bd6b5bbdc49c060364df7835a5826cc2241
Guillermo A. Perez <gaperez64@gmail.com>
2024-05-27 15:50:22 +0200

    added csv files for new entries in the arbiter zoo and added a selection script for parity benchmarks, mostly random

---
+++ parity/selectBenchmarks.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import os
+import random
+import shutil
+import sys
+
+# TODO: Make a list of the benchmarks so that we can (un)select them like we
+# do for the TLSF ones?
+
+
+def makeBenchmarks(inputRoot, outputRoot):
+    found = 0
+    selected = 0
+    for root, dirs, fnames in os.walk(inputRoot, topdown=True):
+        dirs[:] = [d for d in dirs if d != outputRoot]
+        for filename in fnames:
+            # Random choice of benchmarks
+            if filename.endswith(".ehoa"):
+                found += 1
+                if bool(random.getrandbits(1)):
+                    selected += 1
+                    fullname = os.path.join(root, filename)
+                    print(f"Selecting benchmark {fullname}")
+                    shutil.copy(fullname,
+                                os.path.join(outputRoot, filename))
+    print(f"Selected {selected} / {found} benchmarks")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Two positional arguments expected: \n"
+              "  (1) the path to the benchmark-family directories\n"
+              "  (2) the path where you want the benchmarks\n"
+              "The output folder should exist.",
+              file=sys.stderr)
+        exit(1)
+    else:
+        makeBenchmarks(sys.argv[1], sys.argv[2])
+        exit(0)
